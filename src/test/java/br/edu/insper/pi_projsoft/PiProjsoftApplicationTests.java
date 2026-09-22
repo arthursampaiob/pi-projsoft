d32b00170c955cd029443eaa3f8eb76fab3ed1f7
Arthur Sampaio <arthursb07@gmail.com>
2026-09-22 19:57:56 -0300

    configuração inicial aplicação

--- src/test/java/br/edu/insper/pi_projsoft/PiProjsoftApplicationTests.java
+++ src/test/java/br/edu/insper/pi_projsoft/PiProjsoftApplicationTests.java
@@ -1,9 +1,11 @@
 package br.edu.insper.pi_projsoft;
+import org.springframework.test.context.ActiveProfiles;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PiProjsoftApplicationTests {
 
 	@Test
