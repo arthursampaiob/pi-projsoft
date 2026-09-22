09f38670a9861aa23f7e9f5fa417ff1172f09ba3
Arthur Sampaio <arthursb07@gmail.com>
2026-09-22 17:24:46 -0300

    inicializando projeto Spring Boot

---
+++ src/main/java/br/edu/insper/pi_projsoft/PiProjsoftApplication.java
@@ -0,0 +1,13 @@
+package br.edu.insper.pi_projsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiProjsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiProjsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/insper/pi_projsoft/PiProjsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.insper.pi_projsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiProjsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
